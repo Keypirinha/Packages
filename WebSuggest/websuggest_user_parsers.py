d8cbd4ea59c01c61e353c25f8f71af749b3ee0fd
Jean-Charles Lefebvre <polyvertex@gmail.com>
2017-10-26 08:25:53 +0200

    WebSuggest: more detailed inline documentation of websuggest_user_parsers

--- WebSuggest/websuggest_user_parsers.py
+++ WebSuggest/websuggest_user_parsers.py
@@ -1,1 +1,24 @@
-# Placeholder for user's implementations of additional response parsers
+#
+# Placeholder Python module for additional response parsers.
+#
+# Copy this file into the "Profile\Packages\WebSuggest" folder so Keypirinha
+# can load your callback(s) at runtime.
+# You may need to create the "WebSuggest" folder first.
+#
+# Your functions can have any arbitrary name that is supported by the Python
+# language. Do not forget to reference them using the *api_parser* setting.
+#
+# Here is how the default "opensearch" parser is implemented:
+#
+#import json
+#import traceback
+#
+#def my_parser(plugin, provider, response):
+#    try:
+#        response = response.decode(encoding="utf-8", errors="strict")
+#        return json.loads(response)[1]
+#    except:
+#        plugin.warn("Failed to parse response from provider {}.".format(
+#                    provider.label))
+#        traceback.print_exc()
+#        return []
