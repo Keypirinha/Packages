7773b5ddd133967a56e946e57f22310252fd9bb5
mrtvola <39985559+mrtvola@users.noreply.github.com>
2018-06-12 22:03:56 +0200

    edge bookmarks - IOError handling

--- Bookmarks/providers/edge.py
+++ Bookmarks/providers/edge.py
@@ -8,7 +8,7 @@ from ._base import Bookmark, BookmarksProviderBase
 class EdgeProvider(BookmarksProviderBase):
     localappdata_dir = None
 
-    SPARTAN_FILE_LOCATION = r"\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AC\MicrosoftEdge\User\Default\DataStore" \
+    SPARTAN_FILE_LOCATION = r"Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AC\MicrosoftEdge\User\Default\DataStore" \
                             r"\Data\nouser1\120712-0049\DBStore\spartan.edb"
 
     #EDB structure
@@ -26,15 +26,17 @@ class EdgeProvider(BookmarksProviderBase):
 
     def list_bookmarks(self):
         bookmarks = []
-        with open(self.localappdata_dir + self.SPARTAN_FILE_LOCATION, 'rb') as spartan_file:
-            esedb_file = pyesedb.file()
-            esedb_file.open_file_object(spartan_file)
-            favorites_table = esedb_file.get_table_by_name(self.FAVORITES_TABLE_NAME)
-            for i in range(favorites_table.get_number_of_records()):
-                rec = favorites_table.get_record(i)
-                bookmarks.append(Bookmark(self.label,
-                                          rec.get_value_data_as_string(self.TITLE_COLUMN),
-                                          rec.get_value_data_as_string(self.URL_COLUMN)))
-            esedb_file.close()
-
+        try:
+            with open(os.path.join(self.localappdata_dir, self.SPARTAN_FILE_LOCATION), 'rb') as spartan_file:
+                esedb_file = pyesedb.file()
+                esedb_file.open_file_object(spartan_file)
+                favorites_table = esedb_file.get_table_by_name(self.FAVORITES_TABLE_NAME)
+                for i in range(favorites_table.get_number_of_records()):
+                    rec = favorites_table.get_record(i)
+                    bookmarks.append(Bookmark(self.label,
+                                              rec.get_value_data_as_string(self.TITLE_COLUMN),
+                                              rec.get_value_data_as_string(self.URL_COLUMN)))
+                esedb_file.close()
+        except IOError as e:
+            self.plugin.warn("Failed to get Edge bookmarks: {0}".format(str(e)))
         return bookmarks
